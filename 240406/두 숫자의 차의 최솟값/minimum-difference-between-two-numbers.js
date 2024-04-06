cbd8baf33d6c5904a22050c8ba82f6866dbda994
yeseul kim <57187491+ssdd33@users.noreply.github.com>
2024-04-06 22:42:03 +0900

    Title: 두 숫자의 차의 최솟값, Time: 175ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240406/두 숫자의 차의 최솟값/minimum-difference-between-two-numbers.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const [n,ns]=fs.readFileSync('/dev/stdin').toString().split("\n");
+const nums = ns.split(" ").map(v=>+v);
+
+let minDif = nums[1]-nums.shift();
+
+for(let i=0; i<nums.length-1;i++){
+    const dif = nums[i+1]-nums[i];
+if(dif<minDif){
+    minDif = dif;
+}
+}
+
+console.log(minDif)

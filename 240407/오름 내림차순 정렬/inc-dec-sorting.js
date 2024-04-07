e1c3f6eb341c6d2c6b13d33814f4cdc9e60d8cad
yeseul kim <57187491+ssdd33@users.noreply.github.com>
2024-04-07 12:03:09 +0900

    Title: 오름 내림차순 정렬, Time: 187ms, Memory: 15MB, Status: Passed - CodeTree

--- 240407/오름 내림차순 정렬/inc-dec-sorting.js
+++ 240407/오름 내림차순 정렬/inc-dec-sorting.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const input = fs.readFileSync('/dev/stdin').toString().split("\n");
 const n = +input[0];
-const nums = input[1].split(" ").map(v=>+v).sort((a,b)=>a-b);
+const nums = input[1].trim().split(" ").map(v=>+v).sort((a,b)=>a-b);
 
 console.log(nums.join(" "));
 console.log(nums.reverse().join(" "))

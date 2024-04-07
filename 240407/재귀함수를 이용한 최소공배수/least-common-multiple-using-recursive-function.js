ebc4f25d43939ac0b780ca6b4736555f44b1b7e0
yeseul kim <57187491+ssdd33@users.noreply.github.com>
2024-04-07 11:40:24 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 212ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240407/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.js
+++ 240407/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.js
@@ -4,9 +4,11 @@ const n = +input[0];
 const nums = input[1].split(" ").map(v=>+v).sort((a,b)=>a-b);
 
 function solution(num,i){
-    let num2 = nums[i];
-    for(let c = num2; c<=num*num2; c++){
-        if(c%num==0&&c%num2==0){
+    let num1 = nums[i]<num?nums[i]:num;
+    let num2 = num1==num?nums[i]:num;
+
+    for(let c = num2; c<=num1*num2; c++){
+        if(c%num1==0&&c%num2==0){
             if(i!=n-1){
                 solution(c,i+1)
             }else{

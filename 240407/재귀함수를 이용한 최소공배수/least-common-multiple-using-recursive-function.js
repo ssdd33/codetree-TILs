6c6611a60d752d01f788543e1b27da5b452e6528
yeseul kim <57187491+ssdd33@users.noreply.github.com>
2024-04-07 12:03:49 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 191ms, Memory: 14MB, Status: Passed - CodeTree

--- 240407/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.js
+++ 240407/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const input = fs.readFileSync('/dev/stdin').toString().split("\n");
 const n = +input[0];
-const nums = input[1].split(" ").map(v=>+v).sort((a,b)=>a-b);
+const nums = input[1].trim().split(" ").map(v=>+v).sort((a,b)=>a-b);
 
 function solution(num,i){
 if(n==1){

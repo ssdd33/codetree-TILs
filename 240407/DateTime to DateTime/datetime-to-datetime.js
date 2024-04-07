6b3820835ad983273fbf405d1b2e4c7ed6327840
yeseul kim <57187491+ssdd33@users.noreply.github.com>
2024-04-07 14:11:25 +0900

    Title: DateTime to DateTime, Time: 239ms, Memory: 14MB, Status: Passed - CodeTree

--- 240407/DateTime to DateTime/datetime-to-datetime.js
+++ 240407/DateTime to DateTime/datetime-to-datetime.js
@@ -1,8 +1,13 @@
 const fs = require('fs');
 const [a,b,c]=fs.readFileSync('/dev/stdin').toString().trim().split(" ").map(v=>+v);
 
+if(a<11||(a==11&&(b<11||(b==11&&c<11)))){
+    console.log(-1)
+}else{
+
 const minsBefore12 = (24*60) - (11*60+11);
 const d2m = (a-12)*24*60;
 const restMin = b*60+c
 
 console.log(minsBefore12+d2m+restMin)
+}

6360380ee91ee8e375b4d7b9a626647f1b3850c7
yeseul kim <57187491+ssdd33@users.noreply.github.com>
2024-04-07 15:26:34 +0900

    Title: 2진수로 변환하기, Time: 174ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240407/2진수로 변환하기/convert-to-binary.js
+++ 240407/2진수로 변환하기/convert-to-binary.js
@@ -2,9 +2,13 @@ const fs =require('fs');
 let n =+fs.readFileSync('/dev/stdin').toString();
 
 let arr=[];
+if(n==0){
+    console.log(0)
+}else{
 
 while(n>2){
 arr.unshift(n%2);
 n = Math.floor(n/2)
 }
 console.log([1,...arr].join(''));
+}

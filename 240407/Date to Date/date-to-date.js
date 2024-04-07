7702a3b377bcb52c53d26419f2ecf69a70f3be90
yeseul kim <57187491+ssdd33@users.noreply.github.com>
2024-04-07 13:56:45 +0900

    Title: Date to Date, Time: 169ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240407/Date to Date/date-to-date.js
+++ 240407/Date to Date/date-to-date.js
@@ -3,8 +3,12 @@ const [a,b,c,d]=fs.readFileSync('/dev/stdin').toString().trim().split(" ").map(v
 
 const daysOfmonth = [0,31,28,31,30,31,30,31,31,30,31,30,31];
 
-function getDays(m,d){
-    return daysOfmonth.slice(1,m+1).reduce((a,c)=>a+c,0)+d
+if(a==c){
+    console.log(d-b+1);
+}else{
+    let sum = daysOfmonth[a]-b+1+d;
+    for(let m=a+1; m<c;m++){
+        sum+=daysOfmonth[c]
+    }
+    console.log(sum)
 }
-
-console.log(getDays(c,d)-getDays(a,b))

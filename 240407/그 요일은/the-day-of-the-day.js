0c5d7ee8e8ad75736deac28302c677a4cb8b9fe4
yeseul kim <57187491+ssdd33@users.noreply.github.com>
2024-04-07 15:17:56 +0900

    Title: 그 요일은, Time: 172ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240407/그 요일은/the-day-of-the-day.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+    const input = fs.readFileSync('/dev/stdin').toString().split("\n");
+    const [a,b,c,d]=input[0].split(" ").map(v=>+v);
+    const theDay = input[1];
+const day=['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
+const days = [0,31,29,31,30,31,30,31,31,30,31,30,31];
+
+const dif = day.indexOf(theDay)-1;
+let difDays = d-b
+if(a<c){
+
+    difDays += days[a];
+    for(let m = a+1;m<c;m++){
+        difDays+=days[m]
+    }
+}
+
+let times = Math.floor(difDays/7)+(dif<=0?0:difDays%7<=dif?1:0);
+console.log(times);

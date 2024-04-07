eb19e9e7e39a88403a5d427c837cd0ec4b17e2f3
yeseul kim <57187491+ssdd33@users.noreply.github.com>
2024-04-07 12:40:17 +0900

    Title: 비오는 날, Time: 171ms, Memory: 14MB, Status: Passed - CodeTree

--- 240407/비오는 날/rainy-day.js
+++ 240407/비오는 날/rainy-day.js
@@ -12,15 +12,14 @@ rs = rs.map(v=>v.split(" "));
 
 let idxOfday =0;
 for(let i=1; i<rs.length;i++){
-let date1 = new Date(rs[i-1][0]);
+let date1 = new Date(rs[idxOfday][0]);
 let date2 = new Date(rs[i][0]);
 
-if(date1 <date2){
-    idxOfday = i-1;
-}else{
-    idxOfday =i;
+
+if(date1 >date2){
+    idxOfday = i;
 }
 }
-console.log(rs[idxOfday].join(" ").trim())
+console.log(rs[idxOfday].join(" "))
 
 }

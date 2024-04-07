44978acad1775d6f817a74e67e3d7a8a2d4a8009
yeseul kim <57187491+ssdd33@users.noreply.github.com>
2024-04-07 21:54:19 +0900

    Title: 신기한 타일 뒤집기, Time: 283ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 240407/신기한 타일 뒤집기/strange-flipping-tiles.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().split("\n");
+const n = +input[0];
+const p = input.slice(1,1+n).map(v=>v.split(" ")).map(v=>[+v[0],v[1]]);
+
+let arr = Array(100000).fill(2);
+let cti = 50000;
+
+for(let i = 0; i<p.length;i++){
+    const direction = p[i][1];
+    const distance = p[i][0];
+    const code = direction=='L'?1:0;
+    const si = direction=='L'?cti-distance:cti;
+    const se = direction=='L'?cti:cti+distance;
+
+    for(let t = si; t<se;t++){
+        arr[t]=code;
+        
+    }
+    cti = direction=='L'?si:se;
+}
+const wt = arr.filter(v=>v==1);
+const bt = arr.filter(v=>v==0);
+
+console.log(`${wt.length} ${bt.length}`)

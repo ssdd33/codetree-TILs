22b2303ed9dbaffb9cad3bec82b3e54aa1e074d0
yeseul kim <57187491+ssdd33@users.noreply.github.com>
2024-04-08 19:07:37 +0900

    Title: 만나는 그 순간, Time: 5053ms, Memory: 77MB, Status: Time Limit Exceed - CodeTree

--- 240408/만나는 그 순간/the-moment-we-meet.js
+++ 240408/만나는 그 순간/the-moment-we-meet.js
@@ -2,13 +2,14 @@ const fs =require('fs');
 const input = fs.readFileSync("/dev/stdin").toString().split("\n").map(v=>v.split(" "));
 const [n,m]=input[0].map(v=>+v);
 const a = input.slice(1,1+n).map(v=>[v[0],+v[1]]);
-const b = input.slice(n+1).map(v=>[v[0],+v[1]]);
+const b = input.slice(n+1,n+m+1).map(v=>[v[0],+v[1]]);
 
 let atl = [];
 let btl = [];
 
 function makeTimeLine(snapshot){
 const d = snapshot[0]=='R'?1:-1;
+
 return new Array(snapshot[1]).fill(d)
 
 }

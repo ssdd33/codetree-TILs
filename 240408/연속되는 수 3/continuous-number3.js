93c3b4e75f9ff9c493fe095a3ddd5b354ba03936
yeseul kim <57187491+ssdd33@users.noreply.github.com>
2024-04-08 18:01:56 +0900

    Title: 연속되는 수 3, Time: 197ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240408/연속되는 수 3/continuous-number3.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const nums = fs.readFileSync('/dev/stdin').toString().split("\n").map(v=>+v);
+
+let maxCnt = 0;
+let cnt=0;
+
+for(let i=0; i<nums.length; i++){
+    const curNumQ = nums[i]>0;
+    const prevNumQ = nums[i-1]>0;
+    if(i==0||curNumQ!==prevNumQ){
+        if(cnt>maxCnt){
+            maxCnt = cnt;
+        }
+    }else{
+        cnt++
+    }
+}
+
+console.log(maxCnt)

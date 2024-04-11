f273a07705a3f1c55cfd9cb63d85c563ccae53f6
yeseul kim <57187491+ssdd33@users.noreply.github.com>
2024-04-11 13:48:12 +0900

    Title: 되돌아오기, Time: 170ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240411/되돌아오기/come-back.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const input = fs.readFileSync("/dev/stdin").toString().split("\n");
+const n = +input[0];
+const moves = input.slice(1,n+1).map(v=>v.split(" ")).map(v=>[v[0],+v[1]]);
+
+let [x,y,cnt,idx,timer,dir] =[0,0,0,0,0,0];
+const dx =[0,0,1,-1];
+const dy=[1,-1,0,0];
+const d= {'E':0,'W':1,'S':2,'N':3};
+
+while(idx<n){
+
+if(idx>0&&x==0&&y==0){
+console.log(cnt)
+    break;
+} 
+if(timer==0){
+const move = moves[idx];
+dir = d[move[0]];
+timer = move[1];
+idx++;
+}
+x+= dx[dir];
+y+= dy[dir];
+cnt++;
+timer--;
+}
+
+if(x!=0||y!=0){
+
+console.log(-1);
+}

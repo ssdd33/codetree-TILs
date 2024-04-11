0ed603946fdcd8ed09b97a32e3107acf9fb65d43
yeseul kim <57187491+ssdd33@users.noreply.github.com>
2024-04-11 22:55:22 +0900

    Title: 이상한 진수 2, Time: 183ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240411/이상한 진수 2/awkward-digits-2.js
+++ 240411/이상한 진수 2/awkward-digits-2.js
@@ -1,14 +1,28 @@
 const fs =require('fs');
-const n= fs.readFileSync("/dev/stdin").toString().trim().split("").map(v=>+v);
+const str= fs.readFileSync("/dev/stdin").toString().trim();
+const n=str.split("").map(v=>+v);
 
+function getDigit(){
+     let num=0;
+         for(let j=0; j<n.length;j++){
+num = num*2+n[j];
+        }
+        return num;
+}
+if(n.length==1){
+    console.log(n[0]);
+}else {
+    if(str.indexOf("0")==-1){
+n[n.length-1]=0;
+
+
+}else{
 for(let i=0; i<n.length; i++){
     if(n[i]==0){
         n[i]=1;
-        let num=0;
-        for(let j=0; j<n.length;j++){
-num = num*2+n[j];
-        }
-        console.log(num)
         break;
     }
+}
+}
+console.log(getDigit())
 }

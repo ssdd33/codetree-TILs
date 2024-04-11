5a4f08684cb3f37cd7ad72c5015b21dcf4fc3a5a
yeseul kim <57187491+ssdd33@users.noreply.github.com>
2024-04-11 14:09:25 +0900

    Title: 되돌아오기 2, Time: 158ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240411/되돌아오기 2/come-back-2.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const moves = fs.readFileSync("/dev/stdin").toString().trim();
+let n = moves.length;
+let[x,y,dir,idx]=Array(4).fill(0);
+
+const dx = [-1,0,1,0];
+const dy= [0,1,0,-1];
+
+while(idx<n){
+    if(idx>0&&x==0&&y==0){
+        console.log(idx);
+        break;
+    }
+
+    const move = moves[idx];
+    if(move=='F'){
+        x+=dx[dir];
+        y+=dy[dir];
+    }
+    if(move=='R'){
+dir = (dir+1)%4;
+    }
+    if(move=='L'){
+        dir = (dir+3)%4;
+    }
+    idx++;
+}
+
+if(x!=0||y!=0){
+    console.log(-1);
+}

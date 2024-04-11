df90d2b0ef163010fe78b6b847ba30a7cc373358
yeseul kim <57187491+ssdd33@users.noreply.github.com>
2024-04-11 16:20:10 +0900

    Title: 빙빙 돌며 사각형 채우기, Time: 191ms, Memory: 20MB, Status: Wrong Answer - CodeTree

---
+++ 240411/빙빙 돌며 사각형 채우기/snail-alphabet-square.js
@@ -0,0 +1,30 @@
+const fs= require('fs');
+const [n,m]= fs.readFileSync("/dev/stdin").toString().split(" ").map(v=>+v);
+let answer = Array(n).fill(Array(m).fill(''));
+const dx=[0,1,0,-1];
+const dy=[1,0,-1,0];
+let [x,y,d,cnt] =Array(4).fill(0);
+
+function inRange(x,y){
+    return x>=0&&x<n&&y>=0&&y<m;
+}
+
+while(cnt<n*m){
+    const code = cnt<26? cnt+65: (cnt%25)+64;
+    answer[x]=[...answer[x]];
+    answer[x][y]=String.fromCharCode(code);
+
+    let nx= x+dx[d];
+    let ny=y+dy[d];
+    if(!inRange(nx,ny)||answer[nx][ny]!=''){
+        d=(d+1)%4;
+        nx =x+dx[d];
+        ny=y+dy[d];
+    }
+    x=nx;
+    y=ny;
+    cnt++;
+}
+
+
+answer.forEach(r=>console.log(r.join(" ")))

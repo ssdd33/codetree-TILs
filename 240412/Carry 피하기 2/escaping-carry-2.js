eaa9bdb2faadc70a983fd1aa45104e2714b96fe1
yeseul kim <57187491+ssdd33@users.noreply.github.com>
2024-04-12 00:09:18 +0900

    Title: Carry 피하기 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240412/Carry 피하기 2/escaping-carry-2.js
@@ -0,0 +1,29 @@
+const fs =require('fs');
+const input =fs.readFileSync("/dev/stdin").toString().split("\n").map(v=>+v);
+const n = input[0];
+const nums = input.slice(1,n+1);
+
+let maxSum = -1;
+
+function isThatNum(x,y,z){
+    const arrs = arguments.map(n=>n.toString().split("").reverse().map(v=>+v)).sort((a,b)=>a.length-b.length);
+const n = arrs[0].length;
+
+for(let i=0; i<n; i++){
+    let sum = 0;
+    arrs.forEach((n)=>sum+=n[i]);
+    if(sum>=10) return -1;
+}
+return x+y+z;
+}
+
+for(let i = 0; i<n-2; i++){
+    for(let j=i+1; j<n-1; j++){
+        for(let k= j+1; k<n; k++){
+maxSum = Math.max(maxSum,isThatNum(nums[i],nums[j],nums[k]));
+        }
+    }
+}
+
+
+console.log(maxSum)

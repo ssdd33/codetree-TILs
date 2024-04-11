289586deb0aa0085cb64737ac5b3561d60777224
yeseul kim <57187491+ssdd33@users.noreply.github.com>
2024-04-11 14:34:34 +0900

    Title: 격자 위의 편안한 상태, Time: 202ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 240411/격자 위의 편안한 상태/comfortable-state-on-the-grid.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const input = fs.readFileSync("/dev/stdin").toString().split("\n").map(v=>v.split(" ").map(v=>+v));
+const[n,m]=input[0];
+const paints = input.slice(1,1+m);
+let field = Array(n).fill(Array(n).fill(0));
+const dx =[0,0,1,-1];
+const dy=[1,-1,0,0];
+let [cnt,idx]=[0,0]
+
+function inRange(x,y){
+    return x>=0&&x<n&&y>=0&&y<n
+}
+
+while(idx<m){
+const paint = paints[idx];
+const [x,y]=[paint[0]-1,paint[1]-1]
+field[x]=[...field[x]];
+field[x][y]= 1;
+
+let block_cnt = 0;
+for(let i=0;i<4;i++){
+    const nx = x+dx[i];
+    const ny = y+dy[i];
+    if(inRange(nx,ny)&&field[nx][ny]==1){
+        block_cnt++
+    }
+    if(block_cnt==3){
+        console.log(1);
+        break;
+    }
+}
+if(block_cnt!=3){
+    console.log(0);
+}
+idx++;
+}

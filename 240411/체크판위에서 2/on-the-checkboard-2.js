4fca492fde55560cd15ce94cda97e94173da9ec1
yeseul kim <57187491+ssdd33@users.noreply.github.com>
2024-04-11 22:41:12 +0900

    Title: 체크판위에서 2, Time: 167ms, Memory: 17MB, Status: Passed - CodeTree

--- 240411/체크판위에서 2/on-the-checkboard-2.js
+++ 240411/체크판위에서 2/on-the-checkboard-2.js
@@ -4,20 +4,16 @@ const [r,c] = input[0].trim().split(" ").map(v=>+v);
 const grid = input.slice(1,r+1).map(v=>v.split(" "));
 
 let sum =0;
-
+if(grid[0][0]==grid[r-1][c-1]){
+    console.log(0);
+}else{
 for(let i=1; i<r; i++){
     for(let j=1; j<c;j++){
         if(grid[i][j]!=grid[0][0]){
-            for(let k =i+1;k<r;k++){
-                for(let l=j+1;l<c;l++){
+            for(let k =i+1;k<r-1;k++){
+                for(let l=j+1;l<c-1;l++){
                     if(grid[i][j]!=grid[k][l]){
-                        for(let p = k+1;p<r; p++){
-                            for(let q = l+1; q<c; q++){
-                                if(grid[p][q]!=grid[k][l]){
-                                    sum++
-                                }
-                            }
-                        }
+                        sum++
                     }
                 }
             }
@@ -26,3 +22,4 @@ for(let i=1; i<r; i++){
 }
 
 console.log(sum)
+}

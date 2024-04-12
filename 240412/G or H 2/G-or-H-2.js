865f62e6e0616fb0182f257c97198a54acbe2b4b
yeseul kim <57187491+ssdd33@users.noreply.github.com>
2024-04-12 22:29:17 +0900

    Title: G or H 2, Time: 178ms, Memory: 17MB, Status: Wrong Answer - CodeTree

--- 240412/G or H 2/G-or-H-2.js
+++ 240412/G or H 2/G-or-H-2.js
@@ -5,7 +5,7 @@ let seq = input.slice(1,1+n).map(v=>v.split(" ")).map(v=>[+v[0],v[1]]);
 seq = seq.sort((a,b)=>b[0]-a[0]);
 const arr = Array(seq[0][0]+1).fill(0);
 
-let max = seq[0][0];
+let max = seq[0][0]+1;
 
 for(let i=0; i<seq.length;i++){
 arr[seq[i][0]]=seq[i][1];
@@ -14,6 +14,7 @@ arr[seq[i][0]]=seq[i][1];
 while(max>1){
     let done = false;
 for(let i=0; i<arr.length-max+1;i++){
+    if(arr[i]==0||arr[i+max-1]==0)continue
     const section = arr.slice(i,i+max);
     const g_cnt = section.filter(v=>v=='G');
     const h_cnt =section.filter(v=>v=='H');

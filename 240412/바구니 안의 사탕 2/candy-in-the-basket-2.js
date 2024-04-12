2a81b4045557e91bd8ea8265d239e34b95b6094e
yeseul kim <57187491+ssdd33@users.noreply.github.com>
2024-04-12 21:58:31 +0900

    Title: 바구니 안의 사탕 2, Time: 162ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240412/바구니 안의 사탕 2/candy-in-the-basket-2.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const input = fs.readFileSync("/dev/stdin").toString().split("\n").map(v=>v.split(" ").map(v=>+v));
+const [n,k]=input[0];
+let c = input.slice(1,1+n);
+c= c.sort((a,b)=>b[1]-a[1]);
+const arr_length = (2*k)+2>c[0][1]+1?(2*k)+2:c[0][1]+1;
+let arr = Array(arr_length).fill(0);
+
+let max = Number.MIN_SAFE_INTEGER;
+for(let i =0; i<c.length;i++){
+    arr[c[i][1]]=c[i][0];
+}
+
+for(let i = k+1; i<arr_length-k+1;i++){
+const section = arr.slice(i-k,i+k+1);
+
+max = Math.max(max,section.reduce((a,c)=>a+c))
+}
+
+console.log(max)

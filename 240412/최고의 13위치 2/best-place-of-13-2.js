ca6ccb5218e5f7009708041137b1f7c1719d647b
yeseul kim <57187491+ssdd33@users.noreply.github.com>
2024-04-12 20:39:37 +0900

    Title: 최고의 13위치 2, Time: 239ms, Memory: 17MB, Status: Passed - CodeTree

--- 240412/최고의 13위치 2/best-place-of-13-2.js
+++ 240412/최고의 13위치 2/best-place-of-13-2.js
@@ -2,19 +2,21 @@ const fs =require('fs');
 const input = fs.readFileSync("/dev/stdin").toString().split("\n");
 const n =+input[0];
 const grid = input.slice(1,1+n).map(v=>v.split(" ").map(v=>+v));
+let max = Number.MIN_SAFE_INTEGER
 
-let max_of_row =[];
-
-for(let i=0; i<n; i++){
-
-    let max_cnt = Number.MIN_SAFE_INTEGER;
-
-    for(let j=0; j<n-2; j++){
-        max_cnt = Math.max(max_cnt,grid[i][j]+grid[i][j+1]+grid[i][j+2]);
-    }
-    max_of_row.push(max_cnt);
+function coinSum(i,j){
+    return grid[i][j]+grid[i][j+1]+grid[i][j+2]
 }
 
-max_of_row = max_of_row.sort((a,b)=>b-a);
+for(let i=0; i<n;i++){
+    for(let j=0; j<n-2;j++){
+        for(let k =0; k<n;k++){
+            for(let l=0;l<n-2;l++){
+                if(i==k&&l<=j+2)continue;
+                max = Math.max(max, coinSum(i,j)+coinSum(k,l))
+            }
+        }
+    }
+}
 
-console.log(max_of_row[0]+max_of_row[1])
+console.log(max)

f7e27d06ed8b1d6afba6841c6ed88958567312fd
yeseul kim <57187491+ssdd33@users.noreply.github.com>
2024-04-12 21:42:47 +0900

    Title: 아름다운 수열 2, Time: 168ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240412/아름다운 수열 2/beautiful-sequence-2.js
@@ -0,0 +1,24 @@
+const fs =require('fs');
+const input = fs.readFileSync("/dev/stdin").toString().split("\n").map(v=>v.split(" ").map(v=>+v));
+const [n,m]=input[0];
+const seqA = input[1];
+const seqB =input[2];
+
+let cnt =0;
+
+const strB = seqB.sort((a,b)=>a-b).join("");
+
+
+function isThatSeq(seq){
+    const str = seq.sort((a,b)=>a-b).join("")
+    
+    return str==strB;
+}
+
+for( let i =0; i<n-m+1;i++){
+    if(isThatSeq(seqA.slice(i,i+m))){
+        cnt++;
+    }
+}
+
+console.log(cnt)

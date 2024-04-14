c7e59abd8da2b599d8224b755b0674906b6e0036
yeseul kim <57187491+ssdd33@users.noreply.github.com>
2024-04-14 14:30:41 +0900

    Title: 개발자의 순위, Time: 179ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240414/개발자의 순위/developer's-rank.js
@@ -0,0 +1,21 @@
+const fs =require('fs');
+const input =fs.readFileSync("/dev/stdin").toString().split("\n").map(v=>v.split(" ").map(v=>+v));
+const [k,n]=input[0];
+const comps =input.slice(1,1+n);
+
+let cnt =0;
+
+for(let i=0; i<n-1;i++){
+    for(let j=i+1;j<n;j++){
+        const d1 = comps[0][i];
+        const d2 = comps[0][j];
+
+    for(let c=1;c<k;c++){
+const comp = comps[c];
+if(comp.indexOf(d1)>comp.indexOf(d2))break;
+if(c==k-1)cnt++;
+    }
+    }
+}
+
+console.log(cnt)

c0544dea9e22ee2e44f1afa86616fd22840396b1
yeseul kim <57187491+ssdd33@users.noreply.github.com>
2024-04-14 14:33:02 +0900

    Title: 개발자의 순위, Time: 184ms, Memory: 14MB, Status: Passed - CodeTree

--- 240414/개발자의 순위/developer's-rank.js
+++ 240414/개발자의 순위/developer's-rank.js
@@ -9,12 +9,16 @@ for(let i=0; i<n-1;i++){
     for(let j=i+1;j<n;j++){
         const d1 = comps[0][i];
         const d2 = comps[0][j];
-
+let isThatComb = true;
     for(let c=1;c<k;c++){
 const comp = comps[c];
-if(comp.indexOf(d1)>comp.indexOf(d2))break;
-if(c==k-1)cnt++;
+if(comp.indexOf(d1)>comp.indexOf(d2)){
+    isThatComb = false;
+    break;
+}
+
     }
+    if(isThatComb)cnt++
     }
 }
 

29abf5136ffc16ec8554eedcfab94217d72f9068
yeseul kim <57187491+ssdd33@users.noreply.github.com>
2024-04-16 22:49:47 +0900

    Title: 야바위, Time: 174ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240416/야바위/ya-rock.js
@@ -0,0 +1,24 @@
+const fs =require('fs');
+const input =fs.readFileSync("/dev/stdin").toString().split("\n");
+const n= +input[0];
+const s = input.slice(1,n+1).map(v=>v.split(" ").map(v=>+v));
+
+let cnts =[];
+let max =0;
+for(let i=1; i<=3; i++){
+    let cups = Array(3).fill(0);
+    cups[i]=1;
+    let cnt =0;
+    for(let j=0;j<n;j++){
+        const [a,b,c]=s[j];
+        const a_r = cups[a];
+        const b_r = cups[b];
+        cups[a]=b_r;
+        cups[b]=a_r;
+        if(cups[c]==1){cnt++}
+    }
+    cnts.push(cnt);
+    max =Math.max(max,cnt);
+}
+
+console.log(cnts.indexOf(max)+1)

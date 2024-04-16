9fc8806f32a91f9ab3fa6277af3fc2cf7fddf2e5
yeseul kim <57187491+ssdd33@users.noreply.github.com>
2024-04-16 19:56:03 +0900

    Title: 소-개팅, Time: 177ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240416/소-개팅/cow-meetings.js
+++ 240416/소-개팅/cow-meetings.js
@@ -5,8 +5,10 @@ const n =+input[0];
  
 
 let  ans=0;
-   const cnt = {};
-    for(let i=0; i<n;i++){
+
+while(p.length>1){
+       const cnt = {};
+    for(let i=0; i<p.length;i++){
         const [e,k,props]=p[i];
         for(let j=0; j<k;j++){
             const prop = props[j];
@@ -20,12 +22,9 @@ let  ans=0;
 
     let sorted_props = Object.keys(cnt);
     sorted_props.sort((a,b)=>cnt[b]-cnt[a]);
-    
-    let idx=0;
-while(p.length>1){
  
-p = p.filter(v=>v[2].includes(sorted_props[idx]));
-idx++;
+p = p.filter(v=>v[2].includes(sorted_props[0])).map(v=>[v[0],v[1]-1,v[2].filter(p=>p!=sorted_props[0])]);
+
 ans++;
     
 }

eeb741b6f6a50d4b5649fcb05175108edec2579c
yeseul kim <57187491+ssdd33@users.noreply.github.com>
2024-04-16 14:14:40 +0900

    Title: 스승의 은혜 3, Time: 252ms, Memory: 17MB, Status: Passed - CodeTree

--- 240416/스승의 은혜 3/the-grace-form-teacher-3.js
+++ 240416/스승의 은혜 3/the-grace-form-teacher-3.js
@@ -3,8 +3,7 @@ const input =fs.readFileSync("/dev/stdin").toString().split("\n").map(v=>v.split
 
 const [n,b]=input[0];
 const sp = input.slice(1,1+n);
-
-sp.sort((a,b)=>a[0]-b[0]||a[1]-b[1]);
+sp.sort((a,b)=>(a[0]+a[1])-(b[0]+b[1])||a[0]-b[0]);
 
 let max =0;
 
@@ -12,6 +11,7 @@ for(let i=n-1;i>=0;i--){
     const [p,s] =sp[i];
     let sum = (p/2)+s;
     let cnt = 1;
+
     for(let j=0;j<n;j++){
         if(i==j)continue;
         const [ap,as]=sp[j];

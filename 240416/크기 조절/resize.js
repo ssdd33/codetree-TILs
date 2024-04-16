d45876511096950ec3d5d2b289a25af0ad767f1f
yeseul kim <57187491+ssdd33@users.noreply.github.com>
2024-04-16 16:32:46 +0900

    Title: 크기 조절, Time: 176ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240416/크기 조절/resize.js
+++ 240416/크기 조절/resize.js
@@ -5,29 +5,30 @@ let [a,b] =fs.readFileSync("/dev/stdin").toString().split(" ").map(v=>+v);
 if(a==b){
     console.log(0);
 }else{
-    a++;
-    let cnt=1;
-    let inc =1;
+    console.log(Math.floor((b-2-a)/2))
+//     a++;
+//     let cnt=1;
+//     let inc =1;
 
 
-    while(a<b){
-let pos_inc = [inc+1,inc,inc-1];
-
-for(let i=0; i<3; i++){
-    const temp_inc =pos_inc[i];
-    const cur_dif = b-a;
-    const next_dif = cur_dif-temp_inc;
-    if(next_dif>=temp_inc||(temp_inc-1<=1&&next_dif==1)||(temp_inc==1&&cur_dif==1)){
-        inc= temp_inc;
-        a+=inc;
-        cnt++;
+//     while(a<b){
+// let pos_inc = [inc+1,inc,inc-1];
+// console.log(a,pos_inc)
+// for(let i=0; i<3; i++){
+//     const temp_inc =pos_inc[i];
+//     const cur_dif = b-a;
+//     const next_dif = cur_dif-temp_inc;
+//     if(next_dif>=temp_inc||(temp_inc-1<=1&&next_dif==1)||(temp_inc==1&&cur_dif==1)){
+//         inc= temp_inc;
+//         a+=inc;
+//         cnt++;
         
-        break;
-    }
+//         break;
+//     }
     
-}
-    }
+// }
+//     }
 
-    console.log(cnt)
+//     console.log(cnt)
 
 }

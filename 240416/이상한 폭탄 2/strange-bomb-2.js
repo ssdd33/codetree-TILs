c9d3fe81ce08bf658b0941bb65a0726aa83488a9
yeseul kim <57187491+ssdd33@users.noreply.github.com>
2024-04-16 13:22:43 +0900

    Title: 이상한 폭탄 2, Time: 180ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240416/이상한 폭탄 2/strange-bomb-2.js
@@ -0,0 +1,15 @@
+const fs =require('fs');
+const input =fs.readFileSync("/dev/stdin").toString().split("\n");
+const [n,k] =input[0].split(" ").map(v=>+v);
+const bs = input.slice(1,1+n).map(v=>+v);
+
+let biggest = -1;
+
+for(let i=0; i<n-k;i++){
+   if(bs[i]==bs[i+k]){
+    biggest = Math.max(biggest,bs[i])
+   }
+   
+}
+
+console.log(biggest)

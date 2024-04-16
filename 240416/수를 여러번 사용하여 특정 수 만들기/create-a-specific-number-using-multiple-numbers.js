1fb64fdca09b59cd2bef2dab4c4ede3e6a4e0e90
yeseul kim <57187491+ssdd33@users.noreply.github.com>
2024-04-16 23:08:49 +0900

    Title: 수를 여러번 사용하여 특정 수 만들기, Time: 178ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240416/수를 여러번 사용하여 특정 수 만들기/create-a-specific-number-using-multiple-numbers.js
@@ -0,0 +1,22 @@
+const fs =require('fs');
+const [a,b,c]= fs.readFileSync("/dev/stdin").toString().split(" ").map(v=>+v);
+
+const max_a = Math.floor(c/a);
+const max_b = Math.floor(c/b);
+
+let max = 0;
+
+for(let i = 0;i<=max_b;i++){
+    if(c-b*i>=a){
+   for(let j=0; j<max_a;j++){
+        const num =b*i + a*j;
+        if(num<=c){
+
+       max=Math.max(max, num)
+        }
+    }
+    }
+ 
+}
+
+console.log(max)

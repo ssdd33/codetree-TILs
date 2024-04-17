ebb9e71a096034cba9ae05dec5090c8f7e11970a
yeseul kim <57187491+ssdd33@users.noreply.github.com>
2024-04-17 19:17:39 +0900

    Title: 훌륭한 점프, Time: 166ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240417/훌륭한 점프/great-jump.js
@@ -0,0 +1,31 @@
+const fs =require('fs');
+const input= fs.readFileSync("/dev/stdin").toString().split("\n").map(v=>v.split(" ").map(Number));
+const [n,k]=input[0];
+const nums = input[1];
+
+function isPossibleMin(x){
+    let possibleIndecies =[];
+    
+    for(let i=0; i<n;i++){
+        if(i==0||i==n-1){
+            possibleIndecies.push(i);
+        }else if(nums[i]<=x){
+          possibleIndecies.push(i)
+        }
+    }
+
+    for(let i =0; i<possibleIndecies.length-1;i++){
+        if(possibleIndecies[i+1]-possibleIndecies[i]>k){
+            return false;
+        }
+    }
+    return true;
+}
+
+for(let i= Math.min(...nums.slice(1,n-1));i<=Math.max(...nums.slice(1,n-1));i++){
+   
+    if(isPossibleMin(i)){
+        console.log(i);
+        break;
+    }
+}

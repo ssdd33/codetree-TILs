063110fd2630166fd70bd026eeb07b023f008b54
yeseul kim <57187491+ssdd33@users.noreply.github.com>
2024-04-17 14:06:25 +0900

    Title: 팀으로 하는 틱택토 2, Time: 175ms, Memory: 14MB, Status: Passed - CodeTree

--- 240417/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.js
+++ 240417/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.js
@@ -3,14 +3,12 @@ const b = fs.readFileSync("/dev/stdin").toString().split("\n").map(v=>Array.from
 
 let cnt =0;
 
-function inRange(x,y){
-    return x>=0&&x<3&&y>=0&&y<3;
-}
 
 for(let i=1; i<9;i++){
     for(let j=i+1;j<=9;j++){
 //---------------------------가로
 let win =false;
+
 for(let x=0;x<3;x++){
     let i_cnt=0;
     let j_cnt=0;
@@ -21,6 +19,7 @@ for(let x=0;x<3;x++){
       if(b[x][y]==i)i_cnt++
       if(b[x][y]==j)j_cnt++
     }
+  
    if(i_cnt>0&&j_cnt>0&&(i_cnt+j_cnt==3)){
     win=true;
     break;
@@ -29,7 +28,7 @@ for(let x=0;x<3;x++){
 if(win){
     
  cnt++;   
-    break;
+    continue;
 }
 //----------------------세로
 for(let y=0;y<3;y++){
@@ -48,9 +47,9 @@ for(let y=0;y<3;y++){
    }
 }
 if(win){
-    
+  
  cnt++;   
-    break;
+    continue;
 }
 //---------------------대각선 오 아래
 
@@ -64,25 +63,24 @@ if(b[k][k]!=i&&b[k][k]!=j){
       if(b[k][k]==j)j_cnt++
 }
 if(i_cnt>0&&j_cnt>0&&(i_cnt+j_cnt==3)){
-    
+  
     cnt++
-    break;
+    continue;
 }
 //---------------------대각선 왼 아래
  i_cnt=0;
  j_cnt=0;
 for(let k=0;k<3;k++){
     if(b[k][2-k]!=i&&b[k][2-k]!=j){
-     
         break;
     }
      if(b[k][2-k]==i)i_cnt++
       if(b[k][2-k]==j)j_cnt++
 }
 if(i_cnt>0&&j_cnt>0&&(i_cnt+j_cnt==3)){
-    
+  
     cnt++
-    break;
+    
 }
 
     }

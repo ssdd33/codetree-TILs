d48b824dc706250e2194bc835fa041852e9c72bd
yeseul kim <57187491+ssdd33@users.noreply.github.com>
2024-04-17 14:40:32 +0900

    Title: 등장하지 않는 문자열의 길이, Time: 182ms, Memory: 14MB, Status: Passed - CodeTree

--- 240417/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.js
+++ 240417/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.js
@@ -3,11 +3,19 @@ const input =fs.readFileSync("/dev/stdin").toString().split("\n");
 const n = +input[0];
 const str = input[1].trim();
 
-for(let i=0;i<n;i++){
-    let p = str.slice(0,i+1);
-    if(str.indexOf(p)==str.lastIndexOf(p)){
-        console.log(i+1)
-        break;
-    }
-
+for(let c=1;c<=n;c++){
+    
+    let isRepeat = false;
+  for(let i=0;i<=n-c;i++){
+    
+const p = str.slice(i,i+c);
+if(str.indexOf(p)!=str.lastIndexOf(p)){
+    isRepeat = true;
+    break;
+}
+  }
+  if(!isRepeat){
+    console.log(c);
+    break;
+  }
 }

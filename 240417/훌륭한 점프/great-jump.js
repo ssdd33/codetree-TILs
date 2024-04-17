55d81ffb572dac9edd4f99ded78b49cd1b4a2298
yeseul kim <57187491+ssdd33@users.noreply.github.com>
2024-04-17 19:20:04 +0900

    Title: 훌륭한 점프, Time: 190ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240417/훌륭한 점프/great-jump.js
+++ 240417/훌륭한 점프/great-jump.js
@@ -22,7 +22,7 @@ function isPossibleMin(x){
     return true;
 }
 
-for(let i= Math.min(...nums.slice(1,n-1));i<=Math.max(...nums.slice(1,n-1));i++){
+for(let i= Math.min(nums[0],nums[n-1]);i<=n;i++){
    
     if(isPossibleMin(i)){
         console.log(i);

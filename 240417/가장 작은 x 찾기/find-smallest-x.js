c3dca9fe0f68c5574ac331d7ae6b79289d58c7fd
yeseul kim <57187491+ssdd33@users.noreply.github.com>
2024-04-17 15:21:35 +0900

    Title: 가장 작은 x 찾기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240417/가장 작은 x 찾기/find-smallest-x.js
@@ -0,0 +1,24 @@
+const fs =require('fs');
+const input =fs.readFileSync("/dev/stdin").toString().split("\n");
+const n = +input[0];
+const c = input.slice(1,n+1).map(v=>v.split(" ").map(Number));
+
+
+
+
+
+for(let i=1;i<11;i++){
+    let isThatNum = true;
+    for(let j=1;j<5;j++){
+const powNum = Math.pow(2,j)*i;
+const[a,b]=c[j-1];
+if(powNum<a||powNum>b){
+    isThatNum =false; 
+    break;
+    }
+    }
+    if(isThatNum){
+        console.log(i);
+        break;
+    }
+}

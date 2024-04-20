411542160fdd496d8c1eeb8fd461882bde727b8b
yeseul kim <57187491+ssdd33@users.noreply.github.com>
2024-04-20 15:07:10 +0900

    Title: 언덕 깎기, Time: 187ms, Memory: 18MB, Status: Passed - CodeTree

--- 240420/언덕 깎기/hill-cutting.js
+++ 240420/언덕 깎기/hill-cutting.js
@@ -1,15 +1,28 @@
 const fs =require('fs');
 const input = fs.readFileSync("/dev/stdin").toString().split("\n").map(Number);
 const n = input[0]
+const k= 17;
 const hs = input.slice(1,1+n);
 hs.sort((a,b)=>a-b);
-const lowest = hs[0];
-const highest = hs[n-1];
-const dif = highest-lowest;
 
-if(dif<=17){console.log(0)}else{
-const rest_dif = dif-17;
-    const half = Math.floor(rest_dif/2);
-    
-    console.log(Math.pow(half,2)+Math.pow(rest_dif-half,2))
+let min = Number.MAX_SAFE_INTEGER;
+
+for(let i=0; i<83;i++){
+
+
+let cost =0;
+
+for(let j=0; j<n; j++){
+    if(hs[j]<i){
+        cost+= (i-hs[j])*(i-hs[j])
+    }
+    if( hs[j]>i+k){
+cost += (hs[j]-i-k)*(hs[j]-i-k)
+    }
 }
+
+min = Math.min(min, cost)
+
+}
+
+console.log(min)

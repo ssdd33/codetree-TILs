1e695bdf92bb6f58cff0e5db08aa6d7680778e23
yeseul kim <57187491+ssdd33@users.noreply.github.com>
2024-04-21 15:12:32 +0900

    Title: 코딩톡, Time: 176ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240421/코딩톡/coding-talk.js
+++ 240421/코딩톡/coding-talk.js
@@ -3,12 +3,16 @@ const input =fs.readFileSync("/dev/stdin").toString().split("\n");
 const [n,m,p]=input[0].split(" ").map(Number);
 const msg = input.slice(1,1+m).map(v=>v.split(" ")).map(v=>[v[0],+v[1]]);
 
-const isRead = Array(n).fill(false);
+if(msg[p-1][1]==0){
+    console.log(0)
+}else{
+   const isRead = Array(n).fill(false);
 
 for(let i=p-1;i<m;i++){
     const [d,u]=msg[i];
-if(i==p-1&&u==0)break;
+
     isRead[d.charCodeAt()-65] =true;
 }
 
-console.log(isRead.map((v,i)=>v?v:String.fromCharCode(i+65)).filter(v=>typeof(v)=="string").join(" "))
+console.log(isRead.map((v,i)=>v?v:String.fromCharCode(i+65)).filter(v=>typeof(v)=="string").join(" ")) 
+}

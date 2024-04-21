1304e1e0c87edf08ac04204046df62cce973bede
yeseul kim <57187491+ssdd33@users.noreply.github.com>
2024-04-21 10:53:32 +0900

    Title: 최대 H 점수 2, Time: 258ms, Memory: 14MB, Status: Passed - CodeTree

--- 240421/최대 H 점수 2/maximum-h-score-2.js
+++ 240421/최대 H 점수 2/maximum-h-score-2.js
@@ -4,12 +4,12 @@ const input =fs.readFileSync("/dev/stdin").toString().split("\n").map(v=>v.split
 const [n,l]=input[0];
 const nums = input[1];
 
-let max_h = 0;
+for(let i=101; i>0; i--){
+    const num = i
 
-for(let i=0; i<n; i++){
-    const num = nums[i];
     const arr =[...nums];
     let cnt = l;
+
     for(let j=0; j<n;j++){
         if(cnt==0)break;
         if(nums[j]+1==num){
@@ -21,9 +21,8 @@ cnt--;
     const h_cnt = arr.filter(v=>v>=num).length;
 
     if(h_cnt>=num){
-        max_h = Math.max(max_h,num)
+console.log(num)
+break;
     }
 
 }
-
-console.log(max_h)

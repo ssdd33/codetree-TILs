23a04cbae96c514183e22f860adf9024728b5248
yeseul kim <57187491+ssdd33@users.noreply.github.com>
2024-04-21 11:07:07 +0900

    Title: 최대 최소간의 차, Time: 228ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 240421/최대 최소간의 차/difference-between-maximum-and-minimum.js
@@ -0,0 +1,26 @@
+const fs =require('fs')
+const input = fs.readFileSync("/dev/stdin").toString().split("\n").map(v=>v.split(" ").map(Number));
+const [n,k]=input[0];
+const nums = input[1];
+
+nums.sort((a,b)=>a-b);
+
+let min = Number.MAX_SAFE_INTEGER;
+
+for(let i=nums[0];i<=nums[n-1];i++){
+let cost =0;
+for(let j=0; j<n; j++){
+    if(nums[j]<i){
+cost += i- nums[j];
+    }
+    if(nums[j]>i+k){
+        cost+=nums[j]-i-k
+    }
+}
+
+ min = Math.min( min,cost)
+}
+
+
+
+console.log(min)

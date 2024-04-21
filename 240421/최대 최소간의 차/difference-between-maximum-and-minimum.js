13a3042752c4fdfc4a92645d93a8e3e3b4324f01
yeseul kim <57187491+ssdd33@users.noreply.github.com>
2024-04-21 11:17:59 +0900

    Title: 최대 최소간의 차, Time: 178ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240421/최대 최소간의 차/difference-between-maximum-and-minimum.js
+++ 240421/최대 최소간의 차/difference-between-maximum-and-minimum.js
@@ -7,7 +7,9 @@ nums.sort((a,b)=>a-b);
 
 let min = Number.MAX_SAFE_INTEGER;
 
-for(let i=nums[0];i<=nums[n-1];i++){
+const min_n = nums[0]>0?nums[0]:1
+
+for(let i=min_n;i<=nums[n-1];i++){
 let cost =0;
 for(let j=0; j<n; j++){
     if(nums[j]<i){

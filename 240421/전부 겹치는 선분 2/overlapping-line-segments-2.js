58a143ca0847f78337556969c71ad5bcd013fdd0
yeseul kim <57187491+ssdd33@users.noreply.github.com>
2024-04-21 14:06:54 +0900

    Title: 전부 겹치는 선분 2, Time: 170ms, Memory: 14MB, Status: Passed - CodeTree

--- 240421/전부 겹치는 선분 2/overlapping-line-segments-2.js
+++ 240421/전부 겹치는 선분 2/overlapping-line-segments-2.js
@@ -3,23 +3,28 @@ const input =fs.readFileSync("/dev/stdin").toString().split("\n");
 const n = +input[0];
 const segs =input.slice(1,1+n).map(v=>v.split(" ").map(Number));
 
-let common = segs[0];
-let cnt =0;
-let possible = true;
-for(let i=1; i<n; i++){
+    let possible = true;
+for(let j =0; j<n;j++){
+ possible = true;
+    let common = segs[j==0?1:0];
+
+    for(let i=0; i<n; i++){
+        if(i==j)continue;
     const [x1,x2]=common;
     const [x3,x4]=segs[i];
     if(x2<x3||x4<x1){
-        if(cnt<1){
-            cnt++
-            continue;
-        }else{
+       
 possible=false;
 break;
-        }
+        
     }else{
         common = [Math.max(x1,x3),Math.min(x2,x4)]
     }
+}
+if(possible){
+    break;
+}
+
 }
 
 if(possible){

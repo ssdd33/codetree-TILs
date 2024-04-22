2fb61cfe17ee1c13aa5bbcc8e88bdaa31afe4493
yeseul kim <57187491+ssdd33@users.noreply.github.com>
2024-04-22 11:34:17 +0900

    Title: X 달리기, Time: 221ms, Memory: 14MB, Status: Passed - CodeTree

--- 240422/X 달리기/x-run.js
+++ 240422/X 달리기/x-run.js
@@ -5,15 +5,27 @@ let cur_v = 1;
 let cur_d = 0;
 let t =0;
 
+function fac(n){
+    let sum=0;
+    for(let i=1;i<=n;i++){
+        sum+=i;
+    }
+    return sum;
+}
+
 while(cur_d<d){
     cur_d+=cur_v;
     t++;
 
-    if(d-cur_d-1>Math.floor(d/2)){
-        cur_v++;
-    }else if(cur_v>1){
-        cur_v--;
-    }
+   if(d-cur_d>fac(cur_v+1)){
+cur_v++;
+   }
+
+   if(d-cur_d<fac(cur_v)){
+    cur_v--;
+   }
+
+    
 }
 
 console.log(t)

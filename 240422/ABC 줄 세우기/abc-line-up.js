76c080d1d5450c6a5bc7443c3b274fd65d1fa367
yeseul kim <57187491+ssdd33@users.noreply.github.com>
2024-04-22 22:59:55 +0900

    Title: ABC 줄 세우기, Time: 319ms, Memory: 19MB, Status: Wrong Answer - CodeTree

--- 240422/ABC 줄 세우기/abc-line-up.js
+++ 240422/ABC 줄 세우기/abc-line-up.js
@@ -3,65 +3,6 @@ const input =fs.readFileSync("/dev/stdin").toString().split("\n");
 const n = +input[0];
 let l = input[1].split(" ")
 
-// let cnt =0;
-
-// function c2i(char){
-//     return char.charCodeAt()-65;
-// }
-
-
-//  let non_seq_cnt = 0;
-
-// while(true){
-//     let prev_is_seq = false;
-   
-// if(non_seq_cnt==0){
-//     for(let i=0;i<n-1;i++){
-//         if((c2i(l[i+1])-c2i(l[i]))==1){
-//            prev_is_seq =true
-//         }else {
-//             non_seq_cnt++;
-//             if(prev_is_seq){
-// const to_be_idx = c2i(l[i+1]);
-// const as_is_idx =i+1;
-// cnt+=Math.abs(to_be_idx-as_is_idx);
-
-// if(as_is_idx>to_be_idx){
-//     // 앞으로 옮겨져야함
-//     l = moveFoward(as_is_idx,to_be_idx);
-// }else if(as_is_idx<to_be_idx){
-//     //뒤로 
-//     l = moveBackward(as_is_idx,to_be_idx)
-// }else{
-//     continue;
-// }
-// break;
-//             }
-//         }
-//     }
-// }
-    
-//     if(non_seq_cnt==0){
-//         break;
-//     }else{
-// for(let i=0;i<n-1;i++){
-// if((c2i(l[i+1])-c2i(l[i]))!=1){
-//     prev_is_seq=false;
-// }else if(!prev_is_seq){
-
-// break;
-// }
-
-// }
-//     }
-// }
-
-// console.log(cnt);
-
-
-
-
-
 function moveFoward(l,as_is_idx,to_be_idx){
 return [...l.slice(0,to_be_idx),l[as_is_idx],...l.slice(to_be_idx,as_is_idx),...l.slice(as_is_idx+1,n)];
 }
@@ -69,9 +10,11 @@ return [...l.slice(0,to_be_idx),l[as_is_idx],...l.slice(to_be_idx,as_is_idx),...
 function moveBackward(l,as_is_idx,to_be_idx){
     return [...l.slice(0,as_is_idx),...l.slice(as_is_idx+1,to_be_idx+1),l[as_is_idx],...l.slice(to_be_idx+1,n)];
 }
-let min =Number.MAX_SAFE_INTEGER;
-function check(seq,cnt){
 
+let min =Number.MAX_SAFE_INTEGER;
+
+function check(seq,cnt){
+if(cnt>15)return -1;
     let isDone=true;
    
     for(let i=0; i<n; i++){
@@ -89,12 +32,14 @@ nl = moveBackward([...seq],i,to_be)
         }
         
   let temp= check(nl,cnt+Math.abs(i-to_be));
+
   if(temp>0){
     min = Math.min(min,temp)
   }
     }
 
     if(isDone){
+ 
  return cnt
 
     }

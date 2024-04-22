5216caccea91794939604a242b1b26007d0c99f6
yeseul kim <57187491+ssdd33@users.noreply.github.com>
2024-04-22 17:14:32 +0900

    Title: 독서실의 거리두기 2, Time: 224ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240422/독서실의 거리두기 2/study-cafe-keeping-distance-2.js
+++ 240422/독서실의 거리두기 2/study-cafe-keeping-distance-2.js
@@ -7,11 +7,20 @@ let maxDist =0;
 let [max_i,max_j] = [-1,-1];
 let d_cnt= 0;
 let cur_s_idx =-1;
+
+function cal_max_d(){
+
+  if(max_i<0){
+    return maxDist*2
+  }
+  return maxDist
+}
+
 for(let i=0; i<n;i++){
   
     if(seats[i]==1){
 
-        if(d_cnt> maxDist){
+        if(d_cnt> cal_max_d()){
             maxDist=d_cnt;
             max_i=cur_s_idx;
             max_j=i;
@@ -21,7 +30,7 @@ for(let i=0; i<n;i++){
            d_cnt=0;
     }else{
   d_cnt++;
-  if(i==n-1&&d_cnt>=maxDist){
+  if(i==n-1&&d_cnt*2>=maxDist){
 maxDist = d_cnt;
 [max_i,max_j]=[max_j,-1]
   }

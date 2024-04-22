dc87a52db894f909dbf1a1dc2bb3f531e13fc3f1
yeseul kim <57187491+ssdd33@users.noreply.github.com>
2024-04-22 23:30:15 +0900

    Title: 두 번째로 작은 수의 위치, Time: 183ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240422/두 번째로 작은 수의 위치/location-of-the-second-smallest-number.js
+++ 240422/두 번째로 작은 수의 위치/location-of-the-second-smallest-number.js
@@ -3,7 +3,7 @@ const input = fs.readFileSync("/dev/stdin").toString().split("\n");
 const n =+input[0];
 const nums = input[1].split(" ").map(Number);
 
-const sorted = [...nums];
+const sorted = [...Array.from(new Set(nums))];
 sorted.sort((a,b)=>a-b);
 
 console.log(nums.indexOf(sorted[1])+1)

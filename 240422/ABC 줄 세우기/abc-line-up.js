24911cf3a2e32d0503b635714050ff70b656ddb4
yeseul kim <57187491+ssdd33@users.noreply.github.com>
2024-04-22 22:43:38 +0900

    Title: ABC 줄 세우기, Time: 5016ms, Memory: 23MB, Status: Time Limit Exceed - CodeTree

--- 240422/ABC 줄 세우기/abc-line-up.js
+++ 240422/ABC 줄 세우기/abc-line-up.js
@@ -60,8 +60,8 @@ let l = input[1].split(" ")
 
 
 
-let min = Number.MAX_SAFE_INTEGER;
-let done = false;
+
+
 function moveFoward(l,as_is_idx,to_be_idx){
 return [...l.slice(0,to_be_idx),l[as_is_idx],...l.slice(to_be_idx,as_is_idx),...l.slice(as_is_idx+1,n)];
 }
@@ -69,9 +69,11 @@ return [...l.slice(0,to_be_idx),l[as_is_idx],...l.slice(to_be_idx,as_is_idx),...
 function moveBackward(l,as_is_idx,to_be_idx){
     return [...l.slice(0,as_is_idx),...l.slice(as_is_idx+1,to_be_idx+1),l[as_is_idx],...l.slice(to_be_idx+1,n)];
 }
+let min =Number.MAX_SAFE_INTEGER;
 function check(seq,cnt){
-    // if(done)return;
+
     let isDone=true;
+   
     for(let i=0; i<n; i++){
         const to_be = seq[i].charCodeAt()-65;
         if(to_be==i)continue;
@@ -86,14 +88,17 @@ nl = moveBackward([...seq],i,to_be)
 
         }
         
-   return check(nl,cnt+Math.abs(i-to_be))
+  let temp= check(nl,cnt+Math.abs(i-to_be));
+  if(temp>0){
+    min = Math.min(min,temp)
+  }
     }
 
     if(isDone){
-  min = Math.min(min,cnt)
+ return cnt
 
     }
-    return;
+   return -1;
 }
 
 check(l,0)

74a03deef942b82621481d35b0ce454cadcc8d5d
yeseul kim <57187491+ssdd33@users.noreply.github.com>
2024-04-23 15:39:42 +0900

    Title: 전부 포함하는 선분, Time: 193ms, Memory: 14MB, Status: Passed - CodeTree

--- 240423/전부 포함하는 선분/all-inclusive-segment.js
+++ 240423/전부 포함하는 선분/all-inclusive-segment.js
@@ -10,7 +10,10 @@ from_start.sort((a,b)=>a[0]-b[0]);
 from_end.sort((a,b)=>a[1]-b[1]);
 
 let d;
-if(from_start[1][0]-from_start[0][0]>from_end[n-1][1]-from_end[n-2][1]){
+if(from_end[n-1].join("")==from_start[0].join("")){
+d= from_end[n-2][1]-from_start[1][0]
+
+}else if(from_start[1][0]-from_start[0][0]>from_end[n-1][1]-from_end[n-2][1]){
 d = from_end[n-1][1]-from_start[1][0]
 }else{
 d = from_end[n-2][1]-from_start[0][0]

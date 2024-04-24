36b40bb504984f2c16c7134e1f84945e7594c3c9
yeseul kim <57187491+ssdd33@users.noreply.github.com>
2024-04-24 18:04:45 +0900

    Title: 거품 정렬 구현, Time: 180ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240424/거품 정렬 구현/implement-bubble-sort.js
@@ -0,0 +1,19 @@
+const fs=require("fs")
+const input=fs.readFileSync("/dev/stdin").toString().split("\n")
+const n=+input[0]
+const nums=input[1].split(" ").map(Number)
+let sorted=true
+do{
+    sorted=true
+for(let i=0;i<nums.length-1;i++){
+
+    if(nums[i]>nums[1+i]){
+        const tmp=nums[i]
+        nums[i]=nums[1+i]
+        nums[1+i]=tmp
+        sorted=false
+    }
+}
+}while(!sorted)
+
+console.log(nums.join(" "))

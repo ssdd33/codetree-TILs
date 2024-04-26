3f403d4a6099d62cbf0672e5a6c4f0ea058c01a6
yeseul kim <57187491+ssdd33@users.noreply.github.com>
2024-04-26 16:26:27 +0900

    Title: 선택 정렬 구현, Time: 217ms, Memory: 14MB, Status: Passed - CodeTree

--- 240426/선택 정렬 구현/implement-selection-sort.js
+++ 240426/선택 정렬 구현/implement-selection-sort.js
@@ -1,7 +1,7 @@
 const fs =require('fs');
 const input =fs.readFileSync("/dev/stdin").toString().split("\n");
 const n = +input[0];
-const nums = input[1].split(" ").map(Number);
+const nums = input[1].trim().split(" ").map(Number);
 
 
 for(let i=0; i<n-1; i++){

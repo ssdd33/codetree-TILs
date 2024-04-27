cbe2af670067dd63a1f79186bab00d31314f0842
yeseul kim <57187491+ssdd33@users.noreply.github.com>
2024-04-27 11:17:43 +0900

    Title: 기수 정렬 구현, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240427/기수 정렬 구현/implement-radix-sort.js
+++ 240427/기수 정렬 구현/implement-radix-sort.js
@@ -4,15 +4,16 @@ const n = +input[0];
 let nums = input[1].trim().split(" ");
 const max_l = Math.max(...nums).toString().length;
 nums =nums.map(v=>v.length<max_l?v.padStart(max_l):v)
-
+let from_front = [];
 for(let pos =max_l-1;pos>=0;pos-- ){
     let new_arr = Array(10).fill([]);
     for(let i = 0; i<n; i++){
         const idx = nums[i][pos]==" "?0:+nums[i][pos]
         new_arr[idx] = [...new_arr[idx],nums[i]];
     }
+from_front =[...from_front,...new_arr[0]]
 let store = [];
-    for(let i=0; i<10;i++){
+    for(let i=1; i<10;i++){
         for(let j=0; j<new_arr[i].length;j++){
             store.push(new_arr[i][j])
         }
@@ -20,5 +21,6 @@ let store = [];
 
     nums = store
 }
+nums = [...from_front,...nums]
 
 console.log(nums.map(v=>v.trim()).join(" "))

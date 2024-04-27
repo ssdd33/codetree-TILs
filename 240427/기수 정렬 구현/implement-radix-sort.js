4479bec3c1c94bdf4c0220d6a5dfdc5973b1ca6f
yeseul kim <57187491+ssdd33@users.noreply.github.com>
2024-04-27 11:25:08 +0900

    Title: 기수 정렬 구현, Time: 5016ms, Memory: 57MB, Status: Time Limit Exceed - CodeTree

--- 240427/기수 정렬 구현/implement-radix-sort.js
+++ 240427/기수 정렬 구현/implement-radix-sort.js
@@ -3,6 +3,7 @@ const input =fs.readFileSync("/dev/stdin").toString().split("\n");
 const n = +input[0];
 let nums = input[1].trim().split(" ");
 const max_l = Math.max(...nums).toString().length;
+
 nums =nums.map(v=>v.length<max_l?v.padStart(max_l):v)
 let from_front = [];
 for(let pos =max_l-1;pos>=0;pos-- ){
@@ -11,11 +12,18 @@ for(let pos =max_l-1;pos>=0;pos-- ){
         const idx = nums[i][pos]==" "?0:+nums[i][pos]
         new_arr[idx] = [...new_arr[idx],nums[i]];
     }
-from_front =[...from_front,...new_arr[0]]
+
 let store = [];
-    for(let i=1; i<10;i++){
+    for(let i=0; i<10;i++){
         for(let j=0; j<new_arr[i].length;j++){
+            
+            if(pos>0&&new_arr[i][j][pos-1]==" "){
+           
+                from_front.push(new_arr[i][j])
+            }else{
+
             store.push(new_arr[i][j])
+            }
         }
     }
 

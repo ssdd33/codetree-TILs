a3a263e96f338e9b1b69368ee150ccd67562b56a
yeseul kim <57187491+ssdd33@users.noreply.github.com>
2024-04-27 14:53:43 +0900

    Title: 퀵 정렬 구현, Time: 309ms, Memory: 54MB, Status: Passed - CodeTree

--- 240427/퀵 정렬 구현/implement-quick-sort.js
+++ 240427/퀵 정렬 구현/implement-quick-sort.js
@@ -1,7 +1,7 @@
 const fs =require('fs');
 const input =fs.readFileSync("/dev/stdin").toString().split("\n");
 const n = +input[0];
-let arr = input[1].split(" ").map(Number);
+let arr = input[1].trim().split(" ").map(Number);
 
 
 function swap(a,b){

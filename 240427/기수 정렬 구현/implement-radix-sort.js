177f64b8c18059a933e9dc3f333982b716db32e1
yeseul kim <57187491+ssdd33@users.noreply.github.com>
2024-04-27 11:40:19 +0900

    Title: 기수 정렬 구현, Time: 454ms, Memory: 45MB, Status: Passed - CodeTree

--- 240427/기수 정렬 구현/implement-radix-sort.js
+++ 240427/기수 정렬 구현/implement-radix-sort.js
@@ -6,16 +6,23 @@ const max_l = Math.max(...nums).toString().length;
 
 nums =nums.map(v=>v.length<max_l?v.padStart(max_l):v)
 let from_front = [];
+
 for(let pos =max_l-1;pos>=0;pos-- ){
+
     let new_arr = Array(10).fill([]);
     for(let i = 0; i<nums.length; i++){
         const idx = nums[i][pos]==" "?0:+nums[i][pos]
-        new_arr[idx] = [...new_arr[idx],nums[i]];
+       if(new_arr[idx].length==0){
+        new_arr[idx] = [nums[i]];
+       }else{
+        new_arr[idx].push(nums[i])
+       }
     }
 
 let store = [];
     for(let i=0; i<10;i++){
-        for(let j=0; j<new_arr[i].length;j++){
+        if(pos>0&&i==0){
+            for(let j=0; j<new_arr[i].length;j++){
             
             if(pos>0&&new_arr[i][j][pos-1]==" "){
            
@@ -24,11 +31,18 @@ let store = [];
 
             store.push(new_arr[i][j])
             }
+
+
+        } 
+        }else{
+            store = store.concat(new_arr[i])
         }
+       
     }
 
     nums = store
+
 }
-nums = [...from_front,...nums]
+nums = from_front.concat(nums)
 
 console.log(nums.map(v=>v.trim()).join(" "))

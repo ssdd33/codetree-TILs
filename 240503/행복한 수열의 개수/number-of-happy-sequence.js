6dd7541fabd1ccb1f3d7c7176c25a7ac869397eb
yeseul kim <57187491+ssdd33@users.noreply.github.com>
2024-05-03 19:21:04 +0900

    Title: 행복한 수열의 개수, Time: 194ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240503/행복한 수열의 개수/number-of-happy-sequence.js
+++ 240503/행복한 수열의 개수/number-of-happy-sequence.js
@@ -9,7 +9,7 @@ let cnt =0;
 
 function cnt_arr(arr,idx){
     if(arr[idx]==0){
-    arr = Array(100).fill(0);
+    arr = arr.map(v=>v>=m?v:0);
     arr[idx] =1;
     }else{
         arr[idx]++;
@@ -18,12 +18,14 @@ function cnt_arr(arr,idx){
 }
 
 for(let i=0;i<n;i++){
-    let row_cnt =Array(100).fill(0);
-    let col_cnt =Array(100).fill(0);
+    let row_cnt =Array(101).fill(0);
+    let col_cnt =Array(101).fill(0);
 
     for(let j=0;j<n;j++){
+     
       row_cnt =cnt_arr(row_cnt,g[i][j]);
       col_cnt =cnt_arr(col_cnt,g[j][i]);
+     
     }
 
     cnt+=row_cnt.filter(v=>v>=m).length;

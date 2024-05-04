53224e6d76fef867b8a7c78c96d60b9b167803af
yeseul kim <57187491+ssdd33@users.noreply.github.com>
2024-05-04 11:05:49 +0900

    Title: 금 채굴하기, Time: 171ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 240504/금 채굴하기/gold-mining.js
+++ 240504/금 채굴하기/gold-mining.js
@@ -3,91 +3,35 @@ const input =fs.readFileSync("/dev/stdin").toString().split("\n").map(v=>v.split
 const [n,m]=input[0];
 const g = input.slice(1);
 
-const max_coin = g.reduce((a,c)=>a+(c.filter(v=>v==1).length),0)*m;
-let k = 0;
-let ans = 0;
-
-while(true){
-    const nk=k+1;
-    
-if(Math.pow(nk,2)+Math.pow(nk+1,2)<=max_coin){
-    k++;
-}else{
-    break;
-}
+function getAreaOfK(k){
+    return k*k+(k+1)*(k+1);
 }
 
-function new_grid (k){
-    
-    const ng = Array((2*k)+n).fill(Array(2*k+n).fill(0));
-
-for(let i=0; i<k;i++){
-    ng[i]=[...ng[i]]
-}
-
-for(let i =k;i<n+k;i++){
-    
-    ng[i]= [...ng[i]]
-    for(let j=k;j<n+k;j++){
-        if(g[i-k][j-k]==1){
-            ng[i][j]=1
-        }
-    }
-}
-for(let i =k+n-1;i<2*k+n;i++){
-    ng[i]=[...ng[i]]
-}
-return ng;
-}
-
-
-
-function findG(x,y){
-    let cnt =0;
-    
-    for(let i=0; i<=k;i++){
-     
-        const s = y+(k-i);
-        const e = s+i*2;
-        for(let j =s; j<=e;j++){
-            if(ng[x+i][j]==1)cnt++
+function getNumOfGold(row,col,k){
+    let numOfGold =0;
+    for(let i=0; i<n; i++){
+        for(let j=0;j<n;j++){
+            if(Math.abs(row-i)+Math.abs(col-j)<=k){
+                numOfGold+=g[i][j]
+            }
         }
     }
 
-    for( let i=k+1;i<=2*k;i++ ){
-        const s = y+i-k;
-        const e = s+((2*k)-i)*2;
-        for(let j=s; j<=e;j++){
-            if(ng[x+i][j]==1)cnt++
-        }
-    }
-    return cnt;
+    return numOfGold;
 }
 
+let max = 0;
 
-let ng = new_grid(k);
-
-for(let i =0; i<n;i++){
-    for( let j=0; j<n;j++){
-        const c = findG(i,j);
-        
-        ans = Math.max(ans,findG(i,j))
+for(let i=0; i<n; i++){
+    for(let j=0; j<n;j++){
+for(let k =0; k<=n-1;k++){
+    const numOfGold = getNumOfGold(i,j,k);
+    if(getAreaOfK(k)<=numOfGold*m){
+       
+        max = Math.max(max,numOfGold);
+    }
+}
     }
 }
 
-
-while(ans!=0&&Math.pow(k,2)+Math.pow(k+1,2)>ans*m){
-    
-    k--;
-    ans = 0
-ng = new_grid(k);
-for(let i =0; i<n;i++){
-    for( let j=0; j<n;j++){
-        const c = findG(i,j);
-        
-        ans = Math.max(ans,findG(i,j))
-    }
-}
-}
-
-console.log(ans)
+console.log(max)

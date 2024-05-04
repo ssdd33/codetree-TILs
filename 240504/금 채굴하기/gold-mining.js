ba6afb5306101252b3e2cef5ba7433149bc94340
yeseul kim <57187491+ssdd33@users.noreply.github.com>
2024-05-04 10:13:55 +0900

    Title: 금 채굴하기, Time: 182ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240504/금 채굴하기/gold-mining.js
@@ -0,0 +1,70 @@
+const fs =require('fs');
+const input =fs.readFileSync("/dev/stdin").toString().split("\n").map(v=>v.split(" ").map(Number));
+const [n,m]=input[0];
+const g = input.slice(1);
+
+const max_coin = g.reduce((a,c)=>a+(c.filter(v=>v==1).length),0)*m;
+let k = 0;
+let ans = 0;
+
+while(true){
+    const nk=k+1;
+    
+if(Math.pow(nk,2)+Math.pow(nk+1,2)<=max_coin){
+    k++;
+}else{
+    break;
+}
+}
+
+const ng = Array((2*k)+n).fill(Array(2*k+n).fill(0));
+
+
+for(let i=0; i<k;i++){
+    ng[i]=[...ng[i]]
+}
+
+for(let i =k;i<n+k;i++){
+    ng[i]= [...ng[i]]
+    for(let j=k;j<n+k;j++){
+        if(g[i-k][j-k]==1){
+            ng[i][j]=1
+        }
+    }
+}
+for(let i =k+n-1;i<2*k+n;i++){
+    ng[i]=[...ng[i]]
+}
+
+
+function findG(x,y){
+    let cnt =0;
+    
+    for(let i=0; i<=k;i++){
+     
+        const s = y+(k-i);
+        const e = s+i*2;
+        for(let j =s; j<=e;j++){
+            if(ng[x+i][j]==1)cnt++
+        }
+    }
+
+    for( let i=k+1;i<=2*k;i++ ){
+        const s = y+i-k;
+        const e = s+((2*k)-i)*2;
+        for(let j=s; j<=e;j++){
+            if(ng[x+i][j]==1)cnt++
+        }
+    }
+    return cnt;
+}
+
+for(let i =0; i<n;i++){
+    for( let j=0; j<n;j++){
+        const c = findG(i,j);
+        
+        ans = Math.max(ans,findG(i,j))
+    }
+}
+
+console.log(ans)

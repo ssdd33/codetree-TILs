6d0cca0344c42d594a92d8a1548691a90784d2d1
yeseul kim <57187491+ssdd33@users.noreply.github.com>
2024-05-07 19:30:49 +0900

    Title: 기울어진 직사각형의 회전, Time: 182ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240507/기울어진 직사각형의 회전/rotate-slanted-rectangle.js
@@ -0,0 +1,33 @@
+const fs =require('fs');
+const input =fs.readFileSync("/dev/stdin").toString().split("\n");
+const n= +input[0];
+const m = input.slice(1,1+n).map(v=>v.trim().split(" ").map(Number));
+const [r,c,m1,m2,m3,m4,dr] = input[input.length-1].split(" ").map(Number);
+
+const dx =[-1,-1,1,1];
+const counter_dy =[1,-1,-1,1];
+const clock_dy = [-1,1,1,-1];
+const dy = dr==0?counter_dy:clock_dy;
+const step=[dr==0?m1:m2,dr==0?m2:m1];
+let cnt =0;
+let d =0;
+let prev = 0;
+let [x,y]=[r-1,c-1]
+
+while(cnt<(m1+1)*(m2+1)){
+
+    const dir = d%4
+        for(let s = 0; s<step[dir%2];s++){
+            
+const tmp = m[x][y];
+if(prev>0){
+    m[x][y] = prev;
+    cnt++;
+}
+[x,y]=[x+dx[dir],y+dy[dir]]
+prev = tmp;
+        }
+        d++
+}
+
+m.forEach(v=>console.log(v.join(" ")))

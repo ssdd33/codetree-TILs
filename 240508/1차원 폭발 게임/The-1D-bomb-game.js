a617db42a7670df77252228196c007960996cd64
yeseul kim <57187491+ssdd33@users.noreply.github.com>
2024-05-08 13:04:58 +0900

    Title: 1차원 폭발 게임, Time: 246ms, Memory: 14MB, Status: Passed - CodeTree

--- 240508/1차원 폭발 게임/The-1D-bomb-game.js
+++ 240508/1차원 폭발 게임/The-1D-bomb-game.js
@@ -4,33 +4,71 @@ const [n,m] = input[0].split(" ").map(Number);
 let bombs = input.slice(1,1+n).map(Number);
 
 
-if(m==1){
-    console.log(0)
-}else{
-    while(true){
-        let tmp =[];
-        let cnt = 1;
-        for(let i=1; i<bombs.length;i++){
-            if(bombs[i]!=bombs[i-1]){
-                if(cnt<m){
-                    tmp = [...tmp,...Array(cnt).fill(bombs[i-1])]
-                }
-                cnt =1;
-            }else{
-                cnt++
-            }
-        }
+// if(m==1){
+//     console.log(0)
+// }else{
+//     while(true){
+//         let tmp =[];
+//         let cnt = 1;
+//         for(let i=1; i<bombs.length;i++){
+//             if(bombs[i]!=bombs[i-1]){
+//                 if(cnt<m){
+//                     tmp = [...tmp,...Array(cnt).fill(bombs[i-1])]
+//                 }
+//                 cnt =1;
+//             }else{
+//                 cnt++
+//             }
+//         }
 
-        if(cnt<m){
-             tmp = [...tmp,...Array(cnt).fill(bombs[bombs.length-1])]
-        }
+//         if(cnt<m){
+//              tmp = [...tmp,...Array(cnt).fill(bombs[bombs.length-1])]
+//         }
 
-        if(tmp.join()==bombs.join()){
-            break;
+//         if(tmp.join()==bombs.join()){
+//             break;
+//         }
+//         bombs = tmp;
+//         console.log(bombs)
+//     }
+
+//     console.log(bombs.length)
+//     bombs.forEach(v=>console.log(v))
+// }
+
+
+function getEndIdxOfExpl(curIdx){
+    for(let endIdx =curIdx+1; endIdx<bombs.length;endIdx++){
+        if(bombs[endIdx]!=bombs[curIdx]){
+            return endIdx-1
         }
-        bombs = tmp;
     }
 
-    console.log(bombs.length)
-    bombs.forEach(v=>console.log(v))
+    return bombs.length-1;
 }
+
+
+
+while(true){
+    let curIdx = 0;
+    let didExplode = false;
+    while(curIdx<bombs.length){
+        const endIdx = getEndIdxOfExpl(curIdx);
+        
+        if(endIdx-curIdx+1>=m){
+            bombs.splice(curIdx,endIdx-curIdx+1);
+            didExplode = true;
+            
+        }else{
+curIdx = endIdx+1;
+        }
+        
+    }
+
+    if(!didExplode){
+        break;
+    }
+}
+
+console.log(bombs.length)
+bombs.forEach(v=>console.log(v))

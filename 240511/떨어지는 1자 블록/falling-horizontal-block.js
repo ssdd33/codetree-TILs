78a72a003d1f3a63deeb08bfa786338d5fdeac6d
yeseul kim <57187491+ssdd33@users.noreply.github.com>
2024-05-11 11:10:51 +0900

    Title: 떨어지는 1자 블록, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240511/떨어지는 1자 블록/falling-horizontal-block.js
+++ 240511/떨어지는 1자 블록/falling-horizontal-block.js
@@ -3,10 +3,10 @@ const input =fs.readFileSync("/dev/stdin").toString().split("\n").map(v=>v.trim(
 const [n,m,k]=input[0];
 const grid = input.slice(1,1+n);
 
-for(let i=1; i<n; i++){
+for(let i=0; i<n; i++){
     let setBlock = false;
     for(let j =k-1; j<k+m-1;j++){
-        if(grid[i][j]!=0){
+        if(grid[i][j]!=0||(j==k+m-2&&i==n-1&&grid[i][j]==0)){
           grid[i-1] = [...grid[i-1].slice(0,k-1),...Array(m).fill(1),...grid[i-1].slice(k-1+m,n)];
           setBlock =true;
           break;

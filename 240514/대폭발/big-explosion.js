6778e60b939ba71df495dc473775d19a9a4be89e
yeseul kim <57187491+ssdd33@users.noreply.github.com>
2024-05-14 12:47:23 +0900

    Title: 대폭발, Time: 204ms, Memory: 18MB, Status: Passed - CodeTree

--- 240514/대폭발/big-explosion.js
+++ 240514/대폭발/big-explosion.js
@@ -10,29 +10,26 @@ function inRange(x,y){
 function expl(x,y,d){
     const dx= [-1,0,1,0];
     const dy= [0,1,0,-1];
-    const tmp =[];
+    
 
     for(let i=0;i<4;i++){
         let [nx,ny] = [x+(dx[i]*d),y+(dy[i]*d)];
         if(inRange(nx,ny)&&grid[nx][ny]==0){
-            tmp.push([nx,ny]);
+            
+            posOfB.push([nx,ny]);
             grid[nx][ny] =1;
         }
     }
 
-    return tmp;
-}
-
-for(let i=1;i<=m;i++){
-    let tmp = [];
-    for(let j=0; j<posOfB.length; j++){
-        const [x,y]=posOfB[j];
-        const distance = Math.pow(2,i-1);
-      tmp = tmp.concat(expl(x,y,distance));
-    }
     
-    posOfB = posOfB.concat(tmp)
-
+}
+let distance = 1;
+for(let i=1;i<=m;i++){
+    const tmp = [...posOfB];
+    for(const [x,y] of tmp){
+      expl(x,y,distance);
+    }
+    distance*=2;
 }
 
 console.log(posOfB.length)

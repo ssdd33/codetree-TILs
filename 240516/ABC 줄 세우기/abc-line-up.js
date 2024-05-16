80d5efa02a9779513028c9a38e17030b74152c37
yeseul kim <57187491+ssdd33@users.noreply.github.com>
2024-05-16 11:03:08 +0900

    Title: ABC 줄 세우기, Time: 248ms, Memory: 14MB, Status: Passed - CodeTree

--- 240516/ABC 줄 세우기/abc-line-up.js
+++ 240516/ABC 줄 세우기/abc-line-up.js
@@ -1,27 +1,20 @@
 const fs =require('fs');
 const input =fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 const n = +input[0];
-const chars = input[1].trim().split(" ");
+let chars = input[1].trim().split(" ");
 
 
 let cnt = 0;
 
-while(true){
-    let done =true;
-
-    for(let i=0; i<n-1;i++){
-        const to_be_idx = chars[i].charCodeAt()-65;
-        if(i!=to_be_idx){
-            done =false;
-            const tmp = chars[i];
-            chars[i]=chars[i+1];
-            chars[i+1]=tmp;
-            cnt++
+ for(let i=0; i<n;i++){
+        const to_be_char = String.fromCharCode(i+65);
+        const to_be_idx = to_be_char.charCodeAt()-65;
+        const as_is_idx = chars.indexOf(to_be_char);
+        if(as_is_idx!=to_be_idx){
+            chars = [...chars.slice(0,to_be_idx),to_be_char,...chars.slice(to_be_idx,as_is_idx),...chars.slice(as_is_idx+1,n)];
+            cnt += Math.abs(as_is_idx-to_be_idx)
         }
+ 
     }
-    if(done){
-        break;
-    }
-}
 
 console.log(cnt)

4852512f9957ea780304bc1b8f2208183b231ed1
yeseul kim <57187491+ssdd33@users.noreply.github.com>
2024-05-20 10:49:15 +0900

    Title: 코딩톡, Time: 178ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240520/코딩톡/coding-talk.js
@@ -0,0 +1,25 @@
+const fs =require('fs');
+const input =fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const [n,m,p] =input[0].trim().split(" ").map(Number);
+const msgs =input.slice(1,1+m).map(v=>v.trim().split(" ")).map(v=>[v[0],+v[1]]);
+
+if(msgs[p-1][1]!=0){
+
+
+let members = [];
+for(let i=0 ;i<n;i++){
+    members.push(String.fromCharCode(65+i));
+}
+
+for(let i=p-1; i<m;i++){
+    const [m,u] =msgs[i]
+
+    members = members.filter(v=>v!=m)
+}
+
+if(p!=1&&msgs[p-1][1]==msgs[p-2][1]){
+    members = members.filter(v=>v!=msgs[p-2][0])
+}
+
+console.log(members.join(" "))
+}
